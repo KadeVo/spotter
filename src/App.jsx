e4a5af52090b4a8fd10fc3bf14c0b3815cdb1943
Kadin Vo <kadinvo@gmail.com>
2024-02-08 18:20:30 +1300

    Added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Login } from './pages/Login'
 import { Home } from './pages/Home'
 import { UserProvider } from './lib/context/user'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const isLoginPage = window.location.pathname === '/login'
@@ -7,7 +9,8 @@ function App() {
   return (
     <div>
       <UserProvider>
-        <Home />
+        <Navbar />
+        <main>{isLoginPage ? <Login /> : <Home />}</main>
       </UserProvider>
     </div>
   )
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import { useUser } from '../lib/context/user'
+
+export function Navbar() {
+  const user = useUser()
+
+  return (
+    <nav>
+      <a href="/">Idea tracker</a>
+      <div>
+        {user.current ? (
+          <>
+            <span>{user.current.email}</span>
+            <button type="button" onClick={() => user.logout()}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <a href="/login">Login</a>
+        )}
+      </div>
+    </nav>
+  )
+}
