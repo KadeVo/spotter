4d38574eb85b6d23c2044a823cdba93779502a75
Kadin Vo <kadinvo@gmail.com>
2024-02-08 18:08:16 +1300

    added appwrite and env

---
+++ src/lib/appwrite.js
@@ -0,0 +1,8 @@
+import { Client, Databases, Account } from 'appwrite'
+import PROJECTID from '../../.env'
+
+const client = new Client()
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject({ PROJECTID }) // Replace with your project ID
+
+export const account = new Account(client)
+export const databases = new Databases(client)
